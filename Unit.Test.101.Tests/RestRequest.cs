b976b6145ab72cd2bdd3d07b98556f0f911d7b38
Hugo Blanco Sandoval <hugo.blanco.sandoval@gmail.com>
2012-10-04 11:05:51 -0700

    Wrong location of Unit.Test.101.Tests

---
+++ Unit.Test.101.Tests/RestRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unit.Test._101.Tests
+{
+    class RestRequest
+    {
+    }
+}
---
+++ Unit.Test.101.Tests/Unit.Test.101.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit.Testing._101;
+
+using RestSharp;
+
+
+namespace Unit.Test._101.Tests
+{
+    [TestClass]
+    public class NumbersClientTests
+    {
+        [TestMethod] //Naming Conventions: MethodName_TestingScenario_ExpectedResult
+        public void GetRandomFact_ReturnsARandomFactAsString_ReturnsAString()
+        {
+            //SETUP
+            var client = new NumbersClient();
+            
+            //EXECUTION
+            var fact = client.GetRandomFact();
+
+            //ASSERTIONS
+            Assert.IsFalse(String.IsNullOrEmpty(fact));
+        }
+    }
+}
