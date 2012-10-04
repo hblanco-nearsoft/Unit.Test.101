ca81d494b9375cb7f4bbfce0ef488ed08fc7db9d
Hugo Blanco Sandoval <hugo.blanco.sandoval@gmail.com>
2012-10-04 10:14:08 -0700

    Initial Commit, contains the code reviewed in the first class

---
+++ NumbersClient.cs
@@ -0,0 +1,46 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit.Testing._101
+{
+    public static class Extensions
+    {
+        public static String GetString(this NumbersClient.NumbersType enumValue)
+        {
+            return enumValue.ToString().ToLower();
+        }
+    }
+
+
+
+    public class NumbersClient
+    {
+        public enum NumbersType
+        {
+            Trivia,  //Default if ommited
+            Math,
+            Date,
+            Year
+        }
+
+
+        public string GetRandomFact()
+        {
+            var numbers = new RestClient("http://numbersapi.com/");
+            var request = new RestRequest("{type}/{number}", Method.GET);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddUrlSegment("number", NumbersType.Date.GetString());
+            request.AddUrlSegment("type", "random");
+
+            var resp = numbers.Execute(request);
+            var content = resp.Content;
+
+            return content;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit.Testing._101
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new NumbersClient();
+            var content = client.GetRandomFact();
+
+            Console.WriteLine(content);
+            Console.ReadKey();
+        }
+    }
+
+    
+}
